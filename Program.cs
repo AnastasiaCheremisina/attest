16d12d848d97149a5cdcf37ebe2034e4fb23a27e
Nastya Cheremisina <vzalenet@yandex.ru>
2024-02-27 23:11:52 +0300

    zadacha1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int M = 2;
+        int N = 7;
+        
+        PrintNumbersInRange(M, N);
+    }
+
+    static void PrintNumbersInRange(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.WriteLine(m);
+            PrintNumbersInRange(m + 1, n);
+        }
+    }
+}
